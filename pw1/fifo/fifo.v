223a3189a3a79204b25a7c4e8b452e8a414c8fc6
Andrea Grillo <andrea.grillo@epfl.ch>
2024-02-27 13:54:25 +0100

    pw1

---
+++ pw1/fifo/fifo.v
@@ -0,0 +1,11 @@
+module fifo #(parameter nrOfEntries = 16,
+              parameter bitWidth = 32)
+             (input wire       clock,
+                               reset,
+                               push,
+                               pop,
+              input wire [bitWidth−1 : 0]   pushData,
+              output wire                   full,
+                                            empty,
+              output wire [bitWidth−1 : 0]  popData);
+endmodule
