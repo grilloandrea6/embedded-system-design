b65b3d52aaf4fb01cbcc870f0c4412c756e7ac44
Andrea Grillo <andrea.grillo@epfl.ch>
2024-02-27 14:18:00 +0100

    try to implement fifo

--- pw1/fifo/fifo.v
+++ pw1/fifo/fifo.v
@@ -1,11 +1,42 @@
-module fifo #(parameter nrOfEntries = 16,
+module fifo #( parameter nrOfEntries = 16,
               parameter bitWidth = 32)
-             (input wire       clock,
+             ( input wire       clock,
                                reset,
                                push,
                                pop,
-              input wire [bitWidth−1 : 0]   pushData,
+              input wire [bitWidth−1:0]   pushData,
               output wire                   full,
                                             empty,
-              output wire [bitWidth−1 : 0]  popData);
+              output wire [bitWidth−1:0]  popData);
+
+    reg [bitwidth-1 : 0] memoryContent [nrOfEntries-1 : 0];
+
+    reg [$clog(nrOfEntries) - 1 : 0] pushCounter = $clog(nrOfEntries)'b0;
+    reg [$clog(nrOfEntries) - 1 : 0] popCounter  = $clog(nrOfEntries)'b0;
+    
+    always @(posedge clock)
+    begin
+        if(push == 1’b1)
+        begin
+            // write pushData into memoryContent(pushCounter)
+            memoryContent[pushCounter] = pushData;
+            // increment pushCounter
+            pushCounter = pushCounter + $clog(nrOfEntries)'b1;
+        end
+
+        if(pop == 1’b1 and pushCounter > popCounter)
+        begin
+            // read memoryCounter(popData) into popData
+            popData = memoryContent[popData];
+            // increment popCounter
+            popCounter = popCounter + $clog(nrOfEntries)'b1;
+        end
+
+        if (reset == 1’b1)
+        begin
+            memoryContent <= 0;  // ToDo check if ok
+        end
+    
+    end
+
 endmodule
