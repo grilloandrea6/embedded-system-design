8583cf9bf2c249364790b96e877b7e388a61b433
Andrea Grillo <andrea.grillo@epfl.ch>
2024-03-19 14:31:48 +0100

    accelerator first version working!

--- virtual_prototype/modules/grayscale/rgb565Grayscalelse.v
+++ virtual_prototype/modules/grayscale/rgb565Grayscalelse.v
@@ -3,8 +3,19 @@ module rgb565Grayscalelse #(parameter [7:0] customInstructionId = 8'd0)
                 input wire [31:0] valueA,
                 input wire [7:0] isId,
                 output wire done,
-                output wire [31:0] result);
+                output reg [31:0] result);
 
+wire s_isMyCi = (isId == customInstructionId) ? start : 1'b0;
 
+assign done = s_isMyCi;
+wire [5:0] red   = {valueA[15:11],1'b0};
+wire [5:0] green =  valueA[10: 5];
+wire [5:0] blue  = {valueA[ 4: 0],1'b0};
+wire [31:0] partial = (red * 54 + green * 183 + blue * 19);
 
+always @*
+    if (s_isMyCi == 1'b0) result <= 32'd0;
+    else begin
+      result[7:0] <= partial[13:6];
+    end
 endmodule
---
+++ virtual_prototype/programms/grayscaleAccelerated/src/grayscale.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <ov7670.h>
+#include <swap.h>
+#include <vga.h>
+
+#define ACCELERATOR 0
+#define PROFILE     1
+
+int main () {
+  volatile uint16_t rgb565[640*480];
+  volatile uint8_t grayscale[640*480];
+  volatile uint32_t result,cycles,stall,idle;
+  volatile unsigned int *vga = (unsigned int *) 0X50000020;
+  camParameters camParams;
+  vga_clear();
+  
+  printf("Initialising camera (this takes up to 3 seconds)!\n" );
+  camParams = initOv7670(VGA);
+  printf("Done!\n" );
+  printf("NrOfPixels : %d\n", camParams.nrOfPixelsPerLine );
+  result = (camParams.nrOfPixelsPerLine <= 320) ? camParams.nrOfPixelsPerLine | 0x80000000 : camParams.nrOfPixelsPerLine;
+  vga[0] = swap_u32(result);
+  printf("NrOfLines  : %d\n", camParams.nrOfLinesPerImage );
+  result =  (camParams.nrOfLinesPerImage <= 240) ? camParams.nrOfLinesPerImage | 0x80000000 : camParams.nrOfLinesPerImage;
+  vga[1] = swap_u32(result);
+  printf("PCLK (kHz) : %d\n", camParams.pixelClockInkHz );
+  printf("FPS        : %d\n", camParams.framesPerSecond );
+  uint32_t * rgb = (uint32_t *) &rgb565[0];
+
+  vga[2] = swap_u32(2);
+  vga[3] = swap_u32((uint32_t) &grayscale[0]);
+
+#if PROFILE
+  // resetting counters
+  asm volatile ("l.nios_rrr r0, r0, %[in2], 0x8" :: [in2] "r" (0xF << 8));
+#endif
+
+  while(1) {
+#if PROFILE
+    // start all counters
+    asm volatile ("l.nios_rrr r0, r0, %[in2], 0x8" :: [in2] "r" (0xF));
+#endif
+
+    takeSingleImageBlocking((uint32_t) rgb);
+    for (int line = 0; line < camParams.nrOfLinesPerImage; line++) {
+      for (int pixel = 0; pixel < camParams.nrOfPixelsPerLine; pixel++) {
+        uint16_t rgb = swap_u16(rgb565[line*camParams.nrOfPixelsPerLine+pixel]);
+/*        uint32_t grayAcc;
+        printf("eddai\n\n");
+        asm volatile("l.nios_rrr %[out1], %[in1], r0, 12 " : [out1] "=r " (grayAcc):[in1] "r" (rgb));
+        uint32_t red1 = ((rgb >> 11) & 0x1F) << 3;
+        uint32_t green1 = ((rgb >> 5) & 0x3F) << 2;
+        uint32_t blue1 = (rgb & 0x1F) << 3;
+        uint32_t gray = ((red1*54+green1*183+blue1*19) >> 8)&0xFF;
+
+        printf("accelerated value: %x - software value %x \n\n", grayAcc, gray);
+        return 0;
+*/
+#if ACCELERATOR        
+        uint32_t gray;
+        asm volatile("l.nios_rrr %[out1], %[in1], r0, 12 " : [out1] "=r " (gray):[in1] "r" (rgb));
+#else
+        uint32_t red1 = ((rgb >> 11) & 0x1F) << 3;
+        uint32_t green1 = ((rgb >> 5) & 0x3F) << 2;
+        uint32_t blue1 = (rgb & 0x1F) << 3;
+        uint32_t gray = ((red1*54+green1*183+blue1*19) >> 8)&0xFF;
+#endif
+        grayscale[line*camParams.nrOfPixelsPerLine+pixel] = gray;
+      }
+    }
+
+#if PROFILE
+    // disable all counters
+    asm volatile("l.nios_rrr r0, r0, %[in2], 0x8" :: [in2] "r" ((uint32_t) (0xF << 4)));
+
+    // read values
+    asm volatile("l.nios_rrr %[out1], %[in1], r0, 0x8 " : [out1] "=r " (cycles):[in1] "r" (0));
+    printf("CPU cycles\t:\t %u\n", cycles);
+
+    asm volatile("l.nios_rrr %[out1], %[in1], r0, 0x8 " : [out1] "=r " (stall):[in1] "r" (1));
+    printf("Stall cycles\t:\t %u\n", stall);
+
+    asm volatile("l.nios_rrr %[out1], %[in1], r0, 0x8 " : [out1] "=r " (idle):[in1] "r" (2));
+    printf("Bus idle\t:\t %u\n", idle);
+
+    asm volatile("l.nios_rrr %[out1], %[in1], r0, 0x8 " : [out1] "=r " (cycles):[in1] "r" (3));
+    printf("CPU cycles\t:\t %u\n\n", cycles);
+
+    // resetting counters
+    asm volatile ("l.nios_rrr r0, r0, %[in2], 0x8" :: [in2] "r" (0xF << 8)); 
+#endif
+
+  }
+}
